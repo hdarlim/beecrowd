01cddb0dff25669de064f74fbf2350c0a1a36797
henrique darlim <65373930+hdarlim@users.noreply.github.com>
2025-11-26 13:16:33 -0300

    Remove commented URL from Busca na Internet.c

---
+++ 2413 - Busca na Internet.c
@@ -0,0 +1,16 @@
+// https://judge.beecrowd.com/pt/problems/view/2413
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int t = 0;
+    int resultado = 0;
+    
+    scanf("%d", &t);
+    resultado = 4 * t;
+    
+    printf("%d\n", resultado);
+ 
+    return 0;
+}
