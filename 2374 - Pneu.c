98a2cc2592cc38306c1614a2d2159838347690b6
henrique darlim <65373930+hdarlim@users.noreply.github.com>
2025-11-27 14:22:32 -0300

    Remove commented URL from Pneu.c
    Removed commented URL from the top of the file.

---
+++ 2374 - Pneu.c
@@ -0,0 +1,19 @@
+// https://judge.beecrowd.com/pt/problems/view/2374
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n = 0;
+    int m = 0;
+    int total = 0;
+    
+    scanf("%d", &n);
+    scanf("%d", &m);
+    
+    total = n - m;
+    
+    printf("%d\n", total);
+ 
+    return 0;
+}
