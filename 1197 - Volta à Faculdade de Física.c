2f4a6360ecbc50cd41c26bff330b1daee2456fdd
henrique darlim <65373930+hdarlim@users.noreply.github.com>
2025-11-29 14:06:19 -0300

    Add problem link to 1197 - Volta à Faculdade de Física

---
+++ 1197 - Volta à Faculdade de Física.c
@@ -0,0 +1,18 @@
+// https://judge.beecrowd.com/pt/problems/view/1197
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    int v = 0;
+    int t = 0;
+    int s = 0;
+    
+    while (scanf("%d %d", &v, &t) != EOF) {
+        s = 2 * v * t;
+        
+        printf("%d\n", s);
+    }
+    
+    return 0;
+}
