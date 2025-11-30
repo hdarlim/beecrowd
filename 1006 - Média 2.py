37cb320349fd7b2103c5e0745b4aa6ff7fabfaf1
henrique darlim <65373930+hdarlim@users.noreply.github.com>
2025-11-30 18:31:03 -0300

    Calculate weighted average of three inputs

---
+++ 1006 - Média 2.py
@@ -0,0 +1,9 @@
+# https://judge.beecrowd.com/pt/problems/view/1006
+
+A = float(input())
+B = float(input())
+C = float(input())
+
+average = ((A * 2) + (B * 3) + (C * 5))/(2 + 3 + 5)
+
+print(f"MEDIA = {average:.1f}")
